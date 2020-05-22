41eedfe48e0ad9e1f58abb46b6304f1fe4328c93
ozenctaskin <HuseyinOzenc.Taskin@pennmedicine.upenn.edu>
2020-05-22 11:51:24 -0400

    Updated the gear

--- fw_gears/vol2surf_frame/map_fs2gifti.py
+++ fw_gears/vol2surf_frame/map_fs2gifti.py
@@ -1,7 +1,7 @@
 import os 
 import nibabel as nb
 
-def map_fs2gifti(fs_gifti_left, fs_gifti_right, standard_mesh_atlases_folder, output_path, resolution='32k'):
+def map_fs2gifti(fs_gifti_left, fs_gifti_right, workbench_path, recon_all_folder, standard_mesh_atlases_folder, output_path, source_space='native', resolution='32k'):
     
     '''
     Maps gifti converted fsaverage surface to FS_LR gifti
@@ -17,38 +17,114 @@ def map_fs2gifti(fs_gifti_left, fs_gifti_right, standard_mesh_atlases_folder, ou
         - resolution = cifti resolution. Default 32k
     '''
     
-    # Create a subfolder for gifti
-    main_output_folder = os.path.join(output_path, 'gifti')
-    if not os.path.exists(main_output_folder):
-        os.system('mkdir %s' % main_output_folder)
+    # Strip the gii get the image name
+    if fs_gifti_left[-3:] == 'gii':
+        left_name = os.path.split(fs_gifti_left)[1]
+        left_name = left_name[:-4]
+    else:
+        raise RuntimeError('This is not a compatible mri image extention')
+        
+    # Strip the gii get the image name
+    if fs_gifti_right[-3:] == 'gii':
+        right_name = os.path.split(fs_gifti_right)[1]
+        right_name = right_name[:-4]
+    else:
+        raise RuntimeError('This is not a compatible mri image extention')
+        
+    if source_space == 'fsaverage':
+        
+        # Set paths to the files required for the operations 
+        current_sphere_left = os.path.join(standard_mesh_atlases_folder, 'resample_fsaverage',
+                                            'fsaverage_std_sphere.L.164k_fsavg_L.surf.gii')
+        current_sphere_right = os.path.join(standard_mesh_atlases_folder, 'resample_fsaverage',
+                                            'fsaverage_std_sphere.R.164k_fsavg_R.surf.gii')    
+        new_sphere_left = os.path.join(standard_mesh_atlases_folder, 'resample_fsaverage',
+                                            'fs_LR-deformed_to-fsaverage.L.sphere.%s_fs_LR.surf.gii' % resolution) 
+        new_sphere_right = os.path.join(standard_mesh_atlases_folder, 'resample_fsaverage',
+                                            'fs_LR-deformed_to-fsaverage.R.sphere.%s_fs_LR.surf.gii' % resolution)     
+        metric_out_left = os.path.join(output_path, 'fs_LR-interpolated.L.%s_%s.func.gii' % (resolution, left_name)) 
+        metric_out_right = os.path.join(output_path, 'fs_LR-interpolated.R.%s_%s.func.gii' % (resolution, right_name))    
+        current_area_left = os.path.join(standard_mesh_atlases_folder, 'resample_fsaverage',
+                                             'fsaverage.L.midthickness_va_avg.164k_fsavg_L.shape.gii') 
+        current_area_right = os.path.join(standard_mesh_atlases_folder, 'resample_fsaverage',
+                                             'fsaverage.R.midthickness_va_avg.164k_fsavg_R.shape.gii')     
+        new_area_left = os.path.join(standard_mesh_atlases_folder, 'resample_fsaverage',
+                                             'fs_LR.L.midthickness_va_avg.%s_fs_LR.shape.gii' % resolution)    
+        new_area_right = os.path.join(standard_mesh_atlases_folder, 'resample_fsaverage',
+                                             'fs_LR.R.midthickness_va_avg.%s_fs_LR.shape.gii' % resolution)     
+       
+        # Assemble the run commands and call  
+        wb_run_left_string = '%s -metric-resample %s %s %s ADAP_BARY_AREA %s -area-metrics %s %s' % (os.path.join(workbench_path, 'wb_command'), fs_gifti_left, current_sphere_left, new_sphere_left, metric_out_left, current_area_left, new_area_left)
+        wb_run_right_string = '%s -metric-resample %s %s %s ADAP_BARY_AREA %s -area-metrics %s %s' % (os.path.join(workbench_path, 'wb_command'), fs_gifti_right, current_sphere_right, new_sphere_right, metric_out_right, current_area_right, new_area_right)
+        
+        os.system(wb_run_left_string)
+        os.system(wb_run_right_string)
+        
+    elif source_space == 'native':
+        
+        # Make temp maps
+        temporary_stuff = os.path.join('/tmp', 'temporary_gifti')
+        os.system('mkdir %s' % temporary_stuff)
+        
+        left_fs_white = os.path.join(recon_all_folder, 'surf', 'lh.white')
+        left_fs_pial = os.path.join(recon_all_folder, 'surf', 'lh.pial') 
+        left_fs_sphere = os.path.join(recon_all_folder, 'surf', 'lh.sphere.reg') 
+        left_new_sphere = os.path.join(standard_mesh_atlases_folder, 'resample_fsaverage', 'fs_LR-deformed_to-fsaverage.L.sphere.%s_fs_LR.surf.gii' % resolution)
+        left_midthickness_current_out = os.path.join(temporary_stuff, 'lh.midthickness.surf.gii')
+        left_midthickness_new_out = os.path.join(temporary_stuff, 'sub.lh.midthickness.%s_fs_LR.surf.gii' % resolution)
+        left_gifti_sphere = os.path.join(temporary_stuff, 'lh.sphere.reg.surf.gii')
+        
+        right_fs_white = os.path.join(recon_all_folder, 'surf', 'rh.white')
+        right_fs_pial = os.path.join(recon_all_folder, 'surf', 'rh.pial')
+        right_fs_sphere = os.path.join(recon_all_folder, 'surf', 'rh.sphere.reg') 
+        right_new_sphere = os.path.join(standard_mesh_atlases_folder, 'resample_fsaverage', 'fs_LR-deformed_to-fsaverage.R.sphere.%s_fs_LR.surf.gii' % resolution)
+        right_midthickness_current_out = os.path.join(temporary_stuff, 'rh.midthickness.surf.gii')
+        right_midthickness_new_out = os.path.join(temporary_stuff, 'sub.rh.midthickness.%s_fs_LR.surf.gii' % resolution)
+        right_gifti_sphere = os.path.join(temporary_stuff, 'rh.sphere.reg.surf.gii')
     
-    # Set paths to the files required for the operations 
-    current_sphere_left = os.path.join(standard_mesh_atlases_folder, 'resample_fsaverage',
-                                        'fsaverage_std_sphere.L.164k_fsavg_L.surf.gii')
-    current_sphere_right = os.path.join(standard_mesh_atlases_folder, 'resample_fsaverage',
-                                        'fsaverage_std_sphere.R.164k_fsavg_R.surf.gii')    
-    new_sphere_left = os.path.join(standard_mesh_atlases_folder, 'resample_fsaverage',
-                                        'fs_LR-deformed_to-fsaverage.L.sphere.%s_fs_LR.surf.gii' % resolution) 
-    new_sphere_right = os.path.join(standard_mesh_atlases_folder, 'resample_fsaverage',
-                                        'fs_LR-deformed_to-fsaverage.R.sphere.%s_fs_LR.surf.gii' % resolution)     
-    metric_out_left = os.path.join(main_output_folder, 'fs_LR-interpolated.L.%s.func.gii' % resolution) 
-    metric_out_right = os.path.join(main_output_folder, 'fs_LR-interpolated.R.%s.func.gii' % resolution)    
-    current_area_left = os.path.join(standard_mesh_atlases_folder, 'resample_fsaverage',
-                                         'fsaverage.L.midthickness_va_avg.164k_fsavg_L.shape.gii') 
-    current_area_right = os.path.join(standard_mesh_atlases_folder, 'resample_fsaverage',
-                                         'fsaverage.R.midthickness_va_avg.164k_fsavg_R.shape.gii')     
-    new_area_left = os.path.join(standard_mesh_atlases_folder, 'resample_fsaverage',
-                                         'fs_LR.L.midthickness_va_avg.%s_fs_LR.shape.gii' % resolution)    
-    new_area_right = os.path.join(standard_mesh_atlases_folder, 'resample_fsaverage',
-                                         'fs_LR.R.midthickness_va_avg.%s_fs_LR.shape.gii' % resolution)     
-   
-    # Assemble the run commands and call  
-    wb_run_left_string = 'wb_command -metric-resample %s %s %s ADAP_BARY_AREA %s -area-metrics %s %s' % (fs_gifti_left, current_sphere_left, new_sphere_left, metric_out_left, current_area_left, new_area_left)
-    wb_run_right_string = 'wb_command -metric-resample %s %s %s ADAP_BARY_AREA %s -area-metrics %s %s' % (fs_gifti_right, current_sphere_right, new_sphere_right, metric_out_right, current_area_right, new_area_right)
-    
-    os.system(wb_run_left_string)
-    os.system(wb_run_right_string)
-    
+        make_surfaces_left = 'export PATH="/freesurfer/bin:$PATH";export PATH="/anaconda3/bin:$PATH";FSLDIR=/fsl;. ${FSLDIR}/etc/fslconf/fsl.sh;PATH=${FSLDIR}/bin:${PATH};export FSLDIR PATH;%s -freesurfer-resample-prep %s %s %s %s %s %s %s' % (os.path.join(workbench_path, 'wb_shortcuts'),
+                                                                                                                                                                                                                                                    left_fs_white, left_fs_pial,
+                                                                                                                                                                                                                                                    left_fs_sphere, left_new_sphere,
+                                                                                                                                                                                                                                                    left_midthickness_current_out,
+                                                                                                                                                                                                                                                    left_midthickness_new_out,
+                                                                                                                                                                                                                                                    left_gifti_sphere)
+        make_surfaces_right = 'export PATH="/freesurfer/bin:$PATH";export PATH="/anaconda3/bin:$PATH";FSLDIR=/fsl;. ${FSLDIR}/etc/fslconf/fsl.sh;PATH=${FSLDIR}/bin:${PATH};export FSLDIR PATH;%s -freesurfer-resample-prep %s %s %s %s %s %s %s' % (os.path.join(workbench_path, 'wb_shortcuts'),
+                                                                                                                                                                                                                                                     right_fs_white, right_fs_pial,
+                                                                                                                                                                                                                                                     right_fs_sphere, right_new_sphere,
+                                                                                                                                                                                                                                                     right_midthickness_current_out,
+                                                                                                                                                                                                                                                     right_midthickness_new_out,
+                                                                                                                                                                                                                                                     right_gifti_sphere) 
+        os.system(make_surfaces_left)
+        os.system(make_surfaces_right)
+        
+        # Make gifti maps
+        fs_gifti_left
+        left_gifti_sphere
+        left_new_sphere
+        metric_out_left = os.path.join(output_path, 'fs_LR.L.%s_%s.func.gii' % (resolution, left_name))
+        left_midthickness_current_out
+        left_midthickness_new_out
+        
+        fs_gifti_right
+        right_gifti_sphere
+        right_new_sphere
+        metric_out_right = os.path.join(output_path, 'fs_LR.R.%s_%s.func.gii' % (resolution, right_name)) 
+        right_midthickness_current_out
+        right_midthickness_new_out
+        
+        map_surfaces_left = '%s -metric-resample %s %s %s ADAP_BARY_AREA %s -area-surfs %s %s' % (os.path.join(workbench_path, 'wb_command'),
+                                                                                                  fs_gifti_left, left_gifti_sphere,
+                                                                                                  left_new_sphere, metric_out_left,
+                                                                                                  left_midthickness_current_out,
+                                                                                                  left_midthickness_new_out)
+        map_surfaces_right = '%s -metric-resample %s %s %s ADAP_BARY_AREA %s -area-surfs %s %s' % (os.path.join(workbench_path, 'wb_command'),
+                                                                                                   fs_gifti_right, right_gifti_sphere,
+                                                                                                   right_new_sphere, metric_out_right,
+                                                                                                   right_midthickness_current_out,
+                                                                                                   right_midthickness_new_out)        
+        os.system(map_surfaces_left)
+        os.system(map_surfaces_right)
+ 
     # Load the files and modify metadata for easy wb_view loading
     metric_out_left_loaded = nb.load(metric_out_left)
     metric_out_right_loaded = nb.load(metric_out_right)
@@ -60,6 +136,6 @@ def map_fs2gifti(fs_gifti_left, fs_gifti_right, standard_mesh_atlases_folder, ou
 
     # Save the final folders
     nb.save(metric_out_left_loaded, metric_out_left)
-    nb.save(metric_out_right_loaded, metric_out_right)
-
+    nb.save(metric_out_right_loaded, metric_out_right)       
+   
     return(metric_out_left, metric_out_right)
--- fw_gears/vol2surf_frame/map_vol2fs.py
+++ fw_gears/vol2surf_frame/map_vol2fs.py
@@ -1,122 +1,32 @@
 import os 
-import nibabel as nb
 
-def map_vol2fs(image_to_interpolate, template_to_interpolate, output_path, freesurfer_subjects_dir, recon_all_folder, input_is_MNI=True, custom_recon_all=False):
+def map_vol2fs(volume_to_interpolate, fs_subject_dir, subject_name, freesurfer_environment_path, native_output_path, fsaverage_output_path, surf_temp='fsaverage'):
     
-    '''
-    Maps volume image to freesurfer fsaverage or a custom surface
-    
-    Inputs:
-        - image_to_interpolate = The main moving image to interpolate to target
-        surface.
-        - template_to_interpolate = High-res template that is in the same 
-        space with the image_to_interpolate. It is not required if you are not
-        mapping R2 maps, but strongly recommended. 'NA' can be passed instead 
-        if it will not be used.
-        - output_path = Path to output files
-        - from_mni_to_fsaverage = If image_to_interpolate is in MNI152 space
-        and you want to map to fsaverage space, set this to true. Extremely 
-        speeds up the process.
-        - recon_all_folder = Set this to a freesurfer recon-all folder. This
-        is the target of the surface mapping. If you want to map to fsaverage
-        set this to freesurfer-dir/subjects/fsaverage?
-        - freesurfer subject dir - The folder containing all the freesurfer subjects
-    
-    Outputs:
-        The main output folder contains the final left/right surface images and
-        a folder for intermediate files. These intermediate files are:
-            
-        - register.dat = Identity registeration file used for surface mapping
-        - <scan_name>0GenericAffine.mat = Linear matrix between the template and 
-        the surface
-        - <scan_name>1Warp.nii.gz = Warp field of template and surface registration
-        - <scan_name>1InverseWarp.nii.gz = Inverse of the warp mentioned above
-        - <scan_name>Warped.nii.gz = Product after applying the generic and warp
-        field to the template image
-        - <scan_name>InverseWarped.nii.gz = Product after applying the inverse
-        of the warp field to the template image        
-        - <scan_name>finalInterpolatedImage.nii.gz = Final image that is 
-        in the space of the volumetric image which is the base for the provided
-        surface target      
-    '''
-    
-    # If template is not passed, use the main image for the operations
-    if template_to_interpolate == 'NA':
-        template_to_interpolate = image_to_interpolate
-    loaded_image = nb.load(image_to_interpolate)
-    image_dimension = len(loaded_image.shape)
-    
-    # Strip the whole path and extention and get the image name
-    base = os.path.basename(template_to_interpolate)
-    if 'nii' in base:
-        image_name = os.path.splitext(base)[0][:-4]
+    # Strip the .nii.gz get the image name
+    fmri_name = os.path.split(volume_to_interpolate)[1]
+    if volume_to_interpolate[-3:] == '.gz':
+        fmri_name = fmri_name[:-7]
+    elif volume_to_interpolate[-3:] == 'nii':
+        fmri_name = fmri_name[:-4]
     else:
-        image_name = os.path.splitext(base)[0]     
+        raise RuntimeError('This is not a compatible mri image extention')
+
+    # Register map to the native surface
+    native_left = os.path.join(native_output_path, 'lh.native.' + fmri_name + '.mgz')
+    native_right = os.path.join(native_output_path, 'rh.native.' + fmri_name + '.mgz')
+    os.system('%s --mov %s --regheader %s --projfrac 0.5 --hemi lh --o %s' % (os.path.join(freesurfer_environment_path, 'mri_vol2surf'),
+                                                                              volume_to_interpolate, subject_name, native_left))
+    os.system('%s --mov %s --regheader %s --projfrac 0.5 --hemi rh --o %s' % (os.path.join(freesurfer_environment_path, 'mri_vol2surf'),
+                                                                              volume_to_interpolate, subject_name, native_right))
     
-    # Convert dimension to ANTs value
-    if image_dimension == 3:
-        image_type = '0' # scalar
-    if image_dimension == 4:
-        image_type = '3' # time-series
-       
-    # Create the main output folder at the specified path
-    main_output_folder = os.path.join(output_path, 'fs-surf')
-    os.system('mkdir %s' % main_output_folder)
+    # Register map to fsaverage
+    fsaverage_left = os.path.join(fsaverage_output_path, 'lh.fsaverage.' + fmri_name + '.mgz')
+    fsaverage_right = os.path.join(fsaverage_output_path, 'rh.fsaverage.' + fmri_name + '.mgz')
+    os.system('%s --mov %s --regheader %s --projfrac 0.5 --trgsubject %s --hemi lh --o %s' % (os.path.join(freesurfer_environment_path, 'mri_vol2surf'),
+                                                                                              volume_to_interpolate, subject_name, surf_temp,
+                                                                                              fsaverage_left))
+    os.system('%s --mov %s --regheader %s --projfrac 0.5 --trgsubject %s --hemi rh --o %s' % (os.path.join(freesurfer_environment_path, 'mri_vol2surf'),
+                                                                                              volume_to_interpolate, subject_name, surf_temp,
+                                                                                              fsaverage_right))
     
-    # Set the output image names
-    right_hemi_output_path_gifti = os.path.join(main_output_folder, image_name + '-rh.gii')
-    left_hemi_output_path_gifti = os.path.join(main_output_folder, image_name + '-lh.gii')
-    right_hemi_output_path_mgz = os.path.join(main_output_folder, image_name + '-rh.mgz')
-    left_hemi_output_path_mgz = os.path.join(main_output_folder, image_name + '-lh.mgz')   
-    
-    # If interpolating from mni to fsaverage, life is easy    
-    if input_is_MNI and custom_recon_all==False:
-        os.system('export FREESURFER_HOME=/freesurfer;/bin/bash -c \'source $FREESURFER_HOME/FreeSurferEnv.sh &>/dev/null\';export SUBJECTS_DIR=%s;/freesurfer/bin/mri_vol2surf --src %s --out %s --hemi rh --mni152reg' % (freesurfer_subjects_dir, image_to_interpolate, right_hemi_output_path_gifti))
-        os.system('export FREESURFER_HOME=/freesurfer;/bin/bash -c \'source $FREESURFER_HOME/FreeSurferEnv.sh &>/dev/null\';export SUBJECTS_DIR=%s;/freesurfer/bin/mri_vol2surf --src %s --out %s --hemi lh --mni152reg' % (freesurfer_subjects_dir, image_to_interpolate, left_hemi_output_path_gifti))
-        os.system('export FREESURFER_HOME=/freesurfer;/bin/bash -c \'source $FREESURFER_HOME/FreeSurferEnv.sh &>/dev/null\';export SUBJECTS_DIR=%s;/freesurfer/bin/mri_vol2surf --src %s --out %s --hemi rh --mni152reg' % (freesurfer_subjects_dir, image_to_interpolate, right_hemi_output_path_mgz))
-        os.system('export FREESURFER_HOME=/freesurfer;/bin/bash -c \'source $FREESURFER_HOME/FreeSurferEnv.sh &>/dev/null\';export SUBJECTS_DIR=%s;/freesurfer/bin/mri_vol2surf --src %s --out %s --hemi lh --mni152reg' % (freesurfer_subjects_dir, image_to_interpolate, left_hemi_output_path_mgz))
-        
-    # Otherwise we need to do a nonlinear warp to the volume that is the base 
-    # of the target surface
-    if custom_recon_all==True:
- 
-        # Create a folder for intermediate files 
-        intermediate_files = os.path.join(output_path, 'intermediate_folders')
-        os.system('mkdir %s' % intermediate_files)
-        
-        # Get some paths for non-linear registration and run it 
-        target = os.path.join(recon_all_folder, 'mri', 'T1.mgz')
-        output_stem_name = os.path.join(intermediate_files, 'volume2orig')
-        os.system('export ANTSPATH=/usr/lib/ants/;export PATH=${ANTSPATH}:$PATH;/usr/lib/ants/antsRegistrationSyN.sh -d 3 -f %s -m %s -o %s -n 6' % (target, template_to_interpolate, output_stem_name))
-        warp_matrix = output_stem_name + '1Warp.nii.gz'
-        linear_matrix = output_stem_name + '0GenericAffine.mat'
-       
-        # Apply warp to the image 
-        final_warped_image = output_stem_name + '_finalInterpolatedImage.nii.gz'
-        os.system('export ANTSPATH=/usr/lib/ants/;export PATH=${ANTSPATH}:$PATH;/usr/lib/ants/antsApplyTransforms -e %s -i %s -r %s -t %s -t %s -o %s' % (image_type, image_to_interpolate,
-                                                                                target, warp_matrix, linear_matrix,
-                                                                                final_warped_image))
-                
-        # Create a register.dat from the FSL identity matrix
-        fsl_identity_matrix = os.path.join(os.popen('echo $FSLDIR').read().strip(), 'etc', 'flirtsch', 'ident.mat')
-        registerdat_path = os.path.join(intermediate_files, 'register.dat')
-        os.system('export FREESURFER_HOME=/freesurfer;/bin/bash -c \'source $FREESURFER_HOME/FreeSurferEnv.sh &>/dev/null\';export SUBJECTS_DIR=%s;/freesurfer/bin/tkregister2 --mov %s --fsl %s --targ %s --noedit --reg %s' % (freesurfer_subjects_dir, final_warped_image, fsl_identity_matrix,
-                                                                                                                                                                                                                                 final_warped_image, registerdat_path))
-        
-        # Create the final surface maps 
-        subject_dir = os.path.dirname(recon_all_folder)
-        subject_name = os.path.basename(recon_all_folder)     
-        os.system('export FREESURFER_HOME=/freesurfer;/bin/bash -c \'source $FREESURFER_HOME/FreeSurferEnv.sh &>/dev/null\';export SUBJECTS_DIR=%s;/freesurfer/bin/mri_vol2surf --mov %s --ref %s --reg %s --sd %s --srcsubject %s --hemi rh --o %s' % (freesurfer_subjects_dir, final_warped_image, final_warped_image, 
-                                                                                                                                                                                                                                                        registerdat_path, subject_dir,
-                                                                                                                                                                                                                                                        subject_name, right_hemi_output_path_gifti))
-        os.system('export FREESURFER_HOME=/freesurfer;/bin/bash -c \'source $FREESURFER_HOME/FreeSurferEnv.sh &>/dev/null\';export SUBJECTS_DIR=%s;/freesurfer/bin/mri_vol2surf --mov %s --ref %s --reg %s --sd %s --srcsubject %s --hemi lh --o %s' % (freesurfer_subjects_dir, final_warped_image, final_warped_image,
-                                                                                                                                                                                                                                                        registerdat_path, subject_dir,
-                                                                                                                                                                                                                                                        subject_name, left_hemi_output_path_gifti))
-        os.system('export FREESURFER_HOME=/freesurfer;/bin/bash -c \'source $FREESURFER_HOME/FreeSurferEnv.sh &>/dev/null\';export SUBJECTS_DIR=%s;/freesurfer/bin/mri_vol2surf --mov %s --ref %s --reg %s --sd %s --srcsubject %s --hemi rh --o %s' % (freesurfer_subjects_dir, final_warped_image, final_warped_image, 
-                                                                                                                                                                                                                                                        registerdat_path, subject_dir,
-                                                                                                                                                                                                                                                        subject_name, right_hemi_output_path_mgz))
-        os.system('export FREESURFER_HOME=/freesurfer;/bin/bash -c \'source $FREESURFER_HOME/FreeSurferEnv.sh &>/dev/null\';export SUBJECTS_DIR=%s;/freesurfer/bin/mri_vol2surf --mov %s --ref %s --reg %s --sd %s --srcsubject %s --hemi lh --o %s' % (freesurfer_subjects_dir, final_warped_image, final_warped_image,
-                                                                                                                                                                                                                                                        registerdat_path, subject_dir,
-                                                                                                                                                                                                                                                        subject_name, left_hemi_output_path_mgz))
-   
-    return (left_hemi_output_path_gifti, right_hemi_output_path_gifti, left_hemi_output_path_mgz, right_hemi_output_path_mgz)
+    return (native_left, native_right, fsaverage_left, fsaverage_right)
