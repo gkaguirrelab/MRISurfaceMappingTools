22939e0a3ec07d5c97c23f6cea6c4a7ff49ad914
ozenc <HuseyinOzenc.Taskin@ pennmedicine.upenn.edu>
2020-09-28 15:35:59 -0400

    Locate file function can now find the fmriprep images

--- fw_gears/vol2surf_frame/locate_file.py
+++ fw_gears/vol2surf_frame/locate_file.py
@@ -3,6 +3,7 @@ import os
 def locate_file(name, path):
     result = []
     for root, dirs, files in os.walk(path):
-        if name in files:
-            result.append(os.path.join(root, name))
+        for i in files:
+            if name in i:
+                result.append(os.path.join(root, i))
     return result
